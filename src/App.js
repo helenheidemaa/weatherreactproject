d57ddf091e929693a030c70849d4cb3bde818e56
helenheidemaa <98057078+helenheidemaa@users.noreply.github.com>
2023-03-28 22:56:43 +0300

    Added axios to show weather in different cities

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
+import react from "react"
 import logo from './logo.svg';
 import './App.css';
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Weather App</h1>
+    <Weather />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Open Source coded by Helen Heidemaa
         </a>
       </header>
     </div>
---
+++ src/Weather.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function SearchWeather() {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "3a94f3778290bfeee61278505dbbe51d";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a City..."
+        onChange={updateCity}
+      />
+      <button type="Submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li> Temperature: {Math.round(weather.temperature)} C </li>
+          <li> Wind speed: {Math.round(weather.wind)} km/h </li>
+          <li> Humidity: {weather.humidity} % </li>
+          <li> Description: {weather.description} </li>
+          
+            <img src={weather.icon} alt="Weather icon" />
+          
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
